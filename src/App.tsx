b370e55cb0c527f2cb32dde15b2484170a96cf41
İzge Bayyurt <griezwahlm@gmail.com>
2024-10-18 20:46:45 -0400

    Adding tabs and playing with colors (it's messy)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ChakraProvider, Box, Text } from "@chakra-ui/react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import "./App.css";
 
 function App(): React.JSX.Element {
@@ -8,6 +9,8 @@ function App(): React.JSX.Element {
             <div className="App">
                 <header className="App-header">
                     <Box
+                        marginBottom="10px"
+                        marginC
                         w="100%"
                         h="100px"
                         bgGradient="linear(to-r, #1928FA, #CC0080 ,yellow.400)"
@@ -24,6 +27,44 @@ function App(): React.JSX.Element {
                         </Text>
                     </Box>
                 </header>
+                <body className="App-navigation">
+                    <Tabs
+                        borderTop="2px"
+                        size="md"
+                        isFitted
+                        variant="enclosed"
+                        bg="#27278d"
+                    >
+                        <TabList>
+                            <Tab
+                                bg="#A0a0f0"
+                                borderColor="#A0a0f0"
+                                _selected={{ color: "white", bg: "#90a0f0" }}
+                            >
+                                Download
+                            </Tab>
+                            <Tab
+                                bg="#A0a0f0"
+                                borderColor="#A0a0f0"
+                                _selected={{ color: "white", bg: "#90a0f0" }}
+                            >
+                                About us
+                            </Tab>
+                        </TabList>
+
+                        <TabPanels>
+                            <TabPanel>
+                                <p>one!</p>
+                            </TabPanel>
+                            <TabPanel>
+                                <p>two!</p>
+                            </TabPanel>
+                            <TabPanel>
+                                <p>three!</p>
+                            </TabPanel>
+                        </TabPanels>
+                    </Tabs>
+                </body>
                 <body className="App-body"></body>
             </div>
         </ChakraProvider>
